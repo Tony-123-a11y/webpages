0cd1598aa66b7e4e31e1cf205fa89c7e74d57c60
Tony-123-a11y <ayushsinghatschool@gmail.com>
2025-04-17 13:07:24 +0530

    first commit

---
+++ cycling club/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      container: {
+        center:true,
+     
+      },
+      fontFamily:{
+        'font-head': 'Roboto Condensed',
+        'head-small':'Oswald'
+      },
+      colors:{
+        'primary-color':'#31854a',
+      }
+    },
+   
+  },
+  plugins: [],
+}
+
